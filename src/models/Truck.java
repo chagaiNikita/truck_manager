d3fd58ed40287d32c83fb83aac2d9e691bfc7fde
chagaiNikita <chagai.nikita@gmail.com>
2024-12-12 10:22:20 +0600

    Создал класс Truck

---
+++ src/models/Truck.java
@@ -0,0 +1,92 @@
+package models;
+
+import exceptions.StateException;
+import states.OnBase;
+import states.State;
+
+public class Truck {
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public Truck() {
+        stateObj = new OnBase();
+    }
+
+    private int id;
+
+    private String name;
+
+    private String driver;
+
+    private transient Driver driverObj;
+
+    private String state;
+
+    private transient State stateObj;
+
+    @Override
+    public String toString() {
+        return String.format("%-12s : %s%n%-12s : %s%n%-12s : %s%n%-12s : %s%n", "#", id, "Truck", name, "Driver", driver, "Truck state", state);
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setStateObj(State stateObj) {
+        this.stateObj = stateObj;
+    }
+
+    public void startDriving() {
+        try {
+            stateObj.startDriving(this);
+            System.out.println("Грузовик отправился в путь");
+        } catch (StateException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    public void startRepair() {
+        try {
+            stateObj.startRepair(this);
+            System.out.println("Грузовик отправился на ремонт");
+        } catch (StateException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void changeDriver(Driver[] drivers) {
+        try {
+            stateObj.changeDriver(this, drivers);
+            System.out.printf("Теперь грузовик %s, ведет водитель %s%n", name, driver);
+
+        } catch (StateException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDriverObj(Driver driverObj) {
+        this.driverObj = driverObj;
+    }
+
+    public Driver getDriverObj() {
+        return driverObj;
+    }
+
+}
